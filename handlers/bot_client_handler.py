5abe9bd1433d1f1fedbc37788332a556f685e426
PLAYER <166725939+player-cli@users.noreply.github.com>
2024-08-12 21:45:30 +0500

    some work

--- handlers/bot_client_handler.py
+++ handlers/bot_client_handler.py
@@ -1,7 +1,35 @@
+import random
+
+from misc import bot_keyboard
+from misc import bot_says
 
 class Client:
 
-    def __init__(self, bot) -> None:
+    def __init__(self, bot,admin: int,  managers: list, last_message_id: int) -> None:
         self.bot = bot
+        self.managers = [x[0] for x in managers]
+        self.last_message_id = last_message_id
+        self.admin = admin
+
+    def _self_order(self, message):
+        self.bot.send_message(message.chat.id, bot_says.order, reply_markup = None)
+        self.bot.register_next_step_handler(message, self.__forward_to_manager)
+
+    def _manager_order(self, message):
+        self.bot.send_message(message.chat.id, bot_says.managerorder, reply_markup = bot_keyboard.startup_bot_keyboard)
+        self.bot.send_message(random.choice(self.managers), text = f"Заявка по китаю от @{message.from_user.username}")
+
+    def _shoes_clean(self, message):
+        self.bot.send_message(message.chat.id, bot_says.shoesclean, reply_markup = bot_keyboard.startup_bot_keyboard)
+        self.bot.send_message(self.managers[0], text = f"Заявка по кроссовкам от @{message.from_user.username}")
+
+    def _qa(self, message):
+        self.bot.send_message(message.chat.id, bot_says.help, reply_markup = bot_keyboard.startup_bot_keyboard)
+
+    def __forward_to_manager(self, message):
+        self.bot.forward_message(random.choice(self.managers), message.chat.id, message.message_id)
+        self.bot.send_message(message.chat.id, "Отправили заявку на покупку, скоро вам напишет менеджер для подтверждения! :3", reply_markup = bot_keyboard.startup_bot_keyboard)
+
+    
 
     
--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # Import main libs
 
 import os # get api token from envirovnment
-import time # need
 import telebot # lib for work with telegram api
 import logging # logs for telebot
 
@@ -20,18 +19,19 @@ bot = telebot.TeleBot(os.getenv("TOKEN")) # init telebot
 logger = telebot.logger # logs for telebot
 admin_cid = int(os.getenv("ADMIN_CID")) # get cid of admin
 
-
-# Init handler vars
-
-client = bot_client_handler.Client(bot) # client handlers
-admin = bot_management_handler.Admin(bot) # admin handlers
-manager = bot_management_handler.Manager(bot) # manager handlers
-
 # Init databases
 
 client_db = database.DataBase("users")
 banned_db = database.DataBase("banned")
 manager_db = database.DataBase("managers")
+
+# Init handler vars
+
+client = bot_client_handler.Client(bot, admin_cid, manager_db._get_list()) # client handlers
+admin = bot_management_handler.Admin(bot) # admin handlers
+manager = bot_management_handler.Manager(bot) # manager handlers
+
+last_message_id = 0
 
 # Work with telegram api
 
@@ -69,11 +69,34 @@ def bot_devel(message):
 
 @bot.message_handler(func=lambda m: True)
 def bot_work_handler(message):
-    pass
+    clients = client_db._get_list()
+    banned = banned_db._get_list()
+    managers = manager_db._get_list()
+    cid = message.chat.id
+    uid = message.from_user.id
+    user = [cid, uid]
+    if cid != admin_cid and user not in managers and user not in banned:
+        match(message.text):
+            case 'Назад':
+                bot.send_message(message.chat.id, text = bot_says.start, reply_markup = bot_keyboard.client_keyboard)
+            case _:
+                bot.send_message(cid, "Нет такой команды!")
+    if user in managers and user not in banned:
+        match(message.text):
+            case 'Назад':
+                bot.send_message(message.chat.id, text = f"Добро пожаловать {message.from_user.username}:{message.from_user.id}, вы являетесь менеджером!", reply_markup = bot_keyboard.manager_keyboard)
+            case _:
+                bot.send_message(cid, "Нет такой команды!")
+    if cid == admin_cid:
+        match(message.text):
+            case 'Назад':
+                bot.send_message(message.chat.id, text = f"Добро пожаловать {message.from_user.username}:{message.from_user.id}, вы являетесь администратором!", reply_markup = bot_keyboard.admin_keyboard)
+            case _:
+                bot.send_message(cid, "Нет такой команды!")
+    if user in banned:
+        bot.send_message(cid, "У вас нет прав пользоваться данным ботом так как вас забанил менеджер/администратор",reply_markup = None)
 
-@bot.message_handler(func=lambda call: True)
-def bot_work_handler(message):
-    pass
+
 
 # Start bot
 
