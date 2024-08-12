fc32f85e7a98375c21de33c149a2671a9f552137
PLAYER <166725939+player-cli@users.noreply.github.com>
2024-08-12 06:38:51 +0500

    dda

--- handlers/bot_management_handler.py
+++ handlers/bot_management_handler.py
@@ -1,9 +1,14 @@
+from telebot.util import async_list
+
 class Admin:
 
     def __init__(self, bot) -> None:
 
         self.bot = bot
+user_ids = [123456, 789012, 345678]  # список ID пользователей
+message = "Привет, это рассылка!"
 
+async_list(bot.send_message, user_ids, message)
 
 class Manager:
 
