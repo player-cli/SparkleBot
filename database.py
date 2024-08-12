cb0597a73aedc0ab4c700122f11d294098422c2f
PLAYER <166725939+player-cli@users.noreply.github.com>
2024-08-12 06:24:05 +0500

    database complete?

--- database.py
+++ database.py
@@ -1,21 +1,32 @@
 import sqlite3
 
-
 class DataBase:
 
     def __init__(self, table_name: str) -> None:
         self.table_name = table_name
-        self.base = sqlite3.connect("./data.db")
+        self.base = sqlite3.connect("./data.db", check_same_thread=False)
         self.cursor = self.base.cursor()
 
     def _add(self, chat_id: int, user_id) -> None:
-        pass
+        self.cursor.execute(f"INSERT INTO {self.table_name} (chat_id, user_id) VALUES (?, ?)", (chat_id, user_id))
+        self.db.commit()
 
-    def _remove(self, chat_id: int, user_id) -> None:
-        pass
+    def _remove(self, user_id) -> None:
+        self.cursor.execute(f"DELETE FROM {self.table_name} WHERE user_id = (?)", (user_id))
+        self.db.commit()
 
     def _get_list(self) -> list:
-        pass
+        self.cursor.execute(f"SELECT * FROM {self.table_name}")
+        row = self.cursor.fetchall()
+        return [[x[1], x[2]] for x in row]
 
     def _ban(self, chat_id: int, user_id) -> None:
-        pass
+        self.cursor.execute("""
+            INSERT INTO banned (chat_id, user_id)
+            SELECT chat_id, user_id
+            FROM users
+            WHERE user_id = (?)
+            RETURNING *;
+            DELETE FROM users
+            WHERE user_id = (SELECT user_id FROM banned WHERE user_id = (?));
+        """, (user_id, user_id))
